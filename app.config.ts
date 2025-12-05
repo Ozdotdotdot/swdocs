6832620c1cd1694dfaf9980bb3fb032251948d30
Ozair Khan <ozairk11@gmail.com>
2025-12-05 16:58:47 -0500

    Fixed the dark and light mode

---
+++ app.config.ts
@@ -0,0 +1,18 @@
+export default defineAppConfig({
+  docus: {
+    title: 'smash.watch',
+    description: 'Documentation for Smash Watch',
+    header: {
+      title: 'smash.watch'
+    }
+  },
+  colorMode: {
+    preference: 'system',
+    fallback: 'dark',
+    disableSwitch: false,
+    classSuffix: ''
+  },
+  site: {
+    name: 'smash.watch'
+  }
+});
